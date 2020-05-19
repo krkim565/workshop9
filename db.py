a4d6758fe2f3e64f0680c0db827e6d301c4cbecd
Kim R <kimberleyrangel1@gmail.com>
2020-05-19 15:57:06 -0400

    1st

---
+++ db.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+uri = os.environ.get('MONGODB_URI', 'mongodb://localhost')
+client = MongoClient(uri)
+db = client.database
+favourites = db.favourites
+
+def add_book(book):
+ if favourites.count_documents({ '_id': book['id'] }, limit = 1) == 0:
+       book['_id'] = book['id']
+       favourites.insert_one(book)
+
+def delete_book(id):
+    favourites.delete_one({'_id': id})
